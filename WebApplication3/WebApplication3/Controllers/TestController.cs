a005f84f22b4f0f43aebd47a67eaf65e84fc189c
Serhiy Ivanochko <ivanochko@itstep.academy>
2026-06-06 11:30:02 +0300

    Added project with Test Controller

---
+++ WebApplication3/WebApplication3/Controllers/TestController.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers;
+
+public class MyModel
+{
+    public string Name { get; set; }
+    
+    public int Age { get; set; }
+}
+
+[ApiController]
+[Route("api/[controller]")]
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<MyModel>> BlaBlaBla(int id)
+    {
+        var model = new MyModel { Name = "John", Age = 20 };
+        
+        if(id <= 0)
+        {
+            return BadRequest();
+        }
+
+        var a = new List<int>().First();
+
+        Created();
+        NoContent();
+        return Ok(model);
+    }
+}
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<MyModel>> BlaBlaBla()
+    {
+        var model = new MyModel {Name = "John", Age = 20};
+        
+        return Ok(model);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> BlaBlaBla4()
+    {
+        return Ok();
+    }
+    
+    [HttpPut]
+    public async Task<IActionResult> BlaBlaBla3()
+    {
+        return Ok();
+    }
+    
+    [HttpPatch]
+    public async Task<IActionResult> BlaBlaBla2()
+    {
+        return Ok();
+    }
+    
+    [HttpDelete]
+    public async Task<IActionResult> BlaBlaBla1()
+    {
+        return Ok();
+    }
+}
